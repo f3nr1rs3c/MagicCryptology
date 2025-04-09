232d430ba0b89b8a89496a9d6044f2c5548978d6
Doğukan Talha İspirli <51451598+f3nr1rs3c@users.noreply.github.com>
2025-04-09 04:25:18 +0300

    Update Magic_Random_Generator.py

--- Magic_Random_Generator.py
+++ Magic_Random_Generator.py
@@ -15,7 +15,7 @@ def clear_screen():
 def print_banner():
     f = Figlet(font='slant', width=100)
     print(Fore.LIGHTMAGENTA_EX + f.renderText('Magic Passwords') + Fore.RESET)
-    print(Fore.RED + "             | - | Made By : Fenrir - Cyber Security Specialist | - |         " + Fore.RESET)
+    print(Fore.RED + "             | - | Made By : Fenrir - Penetration Tester | - |         " + Fore.RESET)
 
 def print_menu():
     print("""
